0b41fceb3c3b3b1690f34e7000bfe08887613bf0
GamZard <mckim9999@gmail.com>
2025-08-03 08:40:54 +0900

    계산기

--- 연습/연습/연습.cpp
+++ 연습/연습/연습.cpp
@@ -20,6 +20,27 @@ void calc() {
 	a = num.top();
 	num.pop();
 	string oper = op.top().o;
+	op.pop();
+
+	if (oper == "+") {
+		result = a + b;
+	} else if (oper == "-") {
+		result = a - b;
+	} else if (oper == "*") {
+		result = a * b;
+	} else if (oper == "/") {
+		if (b == 0) {
+			cout << "0으로 나눌 수 없습니다." << endl;
+			result = 0;
+		} else {
+			result = a / b;
+		}
+	} else {
+		cout << "알 수 없는 연산자입니다: " << oper << endl;
+		result = 0;
+	}
+
+	num.push(result);
 }
 
 int main() {
